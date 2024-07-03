424089582a108394174eb38526fafff211dadb39
Tamas Matyas <matyas_tommy@hotmail.com>
2024-07-03 17:30:50 +0200

    finished monday class

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+
+async function fetchData() {
+  const fetchResult = await fetch("https://swapi.dev/api/people/");
+  const data = await fetchResult.json();
+  console.log(data);
+  // mindenhol elerheto a data a fetchData fuggvenyen barhol
+}
+
+fetchData();
+
+fetch("https://swapi.dev/api/people/")
+  .then((response) => response.json())
+  .then((data) => console.log(data)); // csak a 2. then callback fugvenyben erheto el a data
