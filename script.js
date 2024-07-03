199687cc0d9458af04dd64929f1de16526d73a8b
Tamas Matyas <matyas_tommy@hotmail.com>
2024-07-03 18:20:40 +0200

    finished

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("hello world");
+/* console.log("hello world");
 
 async function fetchData() {
   const fetchResult = await fetch("https://swapi.dev/api/people/");
@@ -12,3 +12,21 @@ fetchData();
 fetch("https://swapi.dev/api/people/")
   .then((response) => response.json())
   .then((data) => console.log(data)); // csak a 2. then callback fugvenyben erheto el a data
+ */
+
+const rootElement = document.querySelector("#root");
+rootElement.insertAdjacentHTML(
+  "beforeend",
+  `
+        <h1>Welcome to our simple web app!</h1>
+    <div>
+      <p>sziasztok</p>
+      <button>gomb</button>
+    </div>
+    `
+);
+
+const bodyElement = document.querySelector("body");
+console.log(bodyElement);
+
+bodyElement.insertAdjacentHTML("beforeend", "<h2>hello world</h2>");
