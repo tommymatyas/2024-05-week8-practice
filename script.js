6af179d4579a67a624570ae64a55f775b1973278
Tamas Matyas <matyas_tommy@hotmail.com>
2024-07-03 18:57:41 +0200

    finished swapi api

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-console.log("hello world");
+// console.log("hello world");
 
 async function fetchData() {
   const fetchResult = await fetch("https://swapi.dev/api/people/");
   const data = await fetchResult.json();
-  console.log(data);
+  const characters = data.results; //characters = array[10]
+  //   console.log(characters);
+
+  const rootElement = document.querySelector("#root");
+  let charactersHtml = "";
+
+  /*   for (let i = 0; i < characters.length; i++) {
+    console.log(characters[i].name);
+
+    charactersHtml +=
+      ("beforeend",
+      `<div class="character">
+        <p class="name">${characters[i].name}</p>
+        <p class="height">${characters[i].height} cm</p>
+        <p class="mass">${characters[i].mass} kg</p>
+        </div>
+        `);
+  } */
+
+  characters.forEach((character) => {
+    charactersHtml += `<div class="character">
+        <p class="name">${character.name}</p>
+        <p class="height">${character.height} cm</p>
+        <p class="mass">${character.mass} kg</p>
+        </div>`;
+  });
+
+  // TODO Homework do the above with MAP
+
+  rootElement.insertAdjacentHTML("beforeend", charactersHtml);
+
   // mindenhol elerheto a data a fetchData fuggvenyen barhol
 }
 
