9129cf151a50c792f2f61811ae505fc9f3857ca3
Tamas Matyas <matyas_tommy@hotmail.com>
2024-07-05 20:09:32 +0200

    added fetch next and some styling

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const characterComponent = (name, height, mass) => {
   return `
   <div class="character">
+  <h2> Character </h2>
+  <h3> Data: </h3>
         <p class="name">${name}</p>
         <p class="height">${height} cm</p>
         <p class="mass">${mass} kg</p>
         </div>`;
 };
+
+const charactersComponent = (charactersData) => `
+<div class="characters">
+${charactersData
+  .map((characterData) =>
+    characterComponent(
+      characterData.name,
+      characterData.height,
+      characterData.mass
+    )
+  )
+  .join(" ")}
+</div>`;
 
 async function fetchData() {
   const fetchResult = await fetch("https://swapi.dev/api/people/");
@@ -14,14 +29,16 @@ async function fetchData() {
 
   const rootElement = document.querySelector("#root");
 
+  rootElement.insertAdjacentHTML("beforeend", charactersComponent(characters));
   rootElement.insertAdjacentHTML(
     "beforeend",
-    characters
-      .map((character) =>
-        characterComponent(character.name, character.height, character.mass)
-      )
-      .join(" ")
+    `<button class="fetch">Load more...</button>`
   );
+  const fetchButtonElement = document.querySelector("button.fetch");
+  fetchButtonElement.addEventListener("click", () =>
+    console.log("fetch next page")
+  );
+  console.log(data.next);
 }
 
 fetchData();
